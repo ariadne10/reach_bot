f9e86aaf6419abb1837f8a9bd26b7c54345aaafc
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-05-02 15:00:52 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,19 +10,6 @@ if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
      dataframe = pd.read_csv(uploaded_file)
      st.write(dataframe)
-     
-df2 = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
-     st.write(dataframe)
-     
-df3 = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
-     st.write(dataframe)
-
 
 
 # df1 = pd.read_excel (r'C:\Users\rmeli\Desktop\Reach Sheets\EU Sales FY21.xlsx')
