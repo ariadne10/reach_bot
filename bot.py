9bc657585ff3a5ae4ca1fb46ff63e9dfd883e137
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-05-02 14:59:31 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,19 +6,19 @@ import streamlit as st
 # Import and organize excel sheets into dataframes
 
 df1 = st.file_uploader("Choose a file")
-     
+if uploaded_file is not None:     
      # Can be used wherever a "file-like" object is accepted:
      dataframe = pd.read_csv(uploaded_file)
      st.write(dataframe)
      
 df2 = st.file_uploader("Choose a file")
-
+if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
      dataframe = pd.read_csv(uploaded_file)
      st.write(dataframe)
      
 df3 = st.file_uploader("Choose a file")
-
+if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
      dataframe = pd.read_csv(uploaded_file)
      st.write(dataframe)
