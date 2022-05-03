1c427cc3dd27ae7552ce0c4cb68c4a9dfb775997
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-05-03 08:42:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,5 +9,11 @@ pip.main(["install", "openpyxl"])
 
 st.title("REACH Reports")
 
+df1 = pd.read_excel("sheets/EU Sales FY21.xlsx")
+st.table(df1)
+
 df2 = pd.read_excel("sheets/SKU list 040522.xlsx")
 st.table(df2)
+
+df3 = pd.read_excel("sheets/REACH Report 04-05-22.xlsx")
+st.table(df3)
