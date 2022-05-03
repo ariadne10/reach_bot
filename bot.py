c41773fb0a8b308b7c81062c9b53586c54ef0baa
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-05-03 10:39:53 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ if df2 is not None:
     print(e)
     df2 = pd.read_excel(df2)
 try:
-  df2 = df2[["Material Number", "FY22 Forecast"]])
+  df2 = df2[["Material Number", "FY22 Forecast"]]
 except Exception as e:
   print(e)
   st.write("Please upload your file")
@@ -65,7 +65,7 @@ if df3 is not None:
     print(e)
     df3 = pd.read_excel(df3)
 try:
-  df3 = (df3[["Material", "Component", "Quantity"]].sort_values(by='Component').reset_index())
+  df3 = df3[["Material", "Component", "Quantity"]].sort_values(by='Component').reset_index()
   df3 = df3.drop(df3.columns[0], inplace=True, axis=1)
   #st.write(df3)
 except Exception as e: 
