f02f582e3b0eafcd4fd3bd0ea38d7afedc836be4
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-05-03 14:20:51 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ try:
   df3 = df3[["Material", "Component", "Quantity"]].sort_values(by='Component').reset_index()
   df3 = df3.drop(df3.columns[0], inplace=True, axis=1)
   
-except:
+except Exception as e:
   print(e)
 
 if df3 is None:
