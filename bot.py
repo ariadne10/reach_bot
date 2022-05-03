b7bfcf3e68ea1f6db02d9efb2308ec65f31d4f64
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-05-03 08:17:43 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 # Import and organize excel sheets into dataframes
 
-uploaded_file = st.file_uploader(“Choose a file”)
+uploaded_file = st.file_uploader("file")
 if uploaded_file is not None:
      df1=pd.read_csv(uploaded_file)
      df1=pd.read_excel(uploaded_file)
