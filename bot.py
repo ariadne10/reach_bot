64b172af4c834c9a76ec22b506925b11679fd2d5
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2022-05-03 12:05:45 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,6 +70,7 @@ try:
   df3 = df3.dropna()
   df3 = df3[["Material", "Component", "Quantity"]].sort_values(by='Component').reset_index()
   df3 = df3.drop(df3.columns[0], inplace=True, axis=1)
+  pass
 except Exception as e:
   print(e)
   st.write("Please upload your files")
